f43c7bf40eeb7fc14aca417ffd688cda337d4de1
mansianadi <166349204+mansianadi@users.noreply.github.com>
2024-10-29 16:30:21 -0400

    Add files via upload

---
+++ ww.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle message visibility
+document.getElementById("toggleButton").addEventListener("click", function() {
+    const message = document.getElementById("toggleMessage");
+    message.classList.toggle("hidden");
+});
